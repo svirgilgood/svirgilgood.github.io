b9790b10ed46d90748a00c016ca3deb8e6122fe3
e2dubba <svirgilgoodwin@gmail.com>
2019-08-07 11:33:35 -0500

    adding script to create new blog posts

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 <ul>
     {% for post in site.posts %}
     <li>
-    <a href="{{ post.url }}">{{post.date}}: {{ post.title }} </a>
+    <a href="{{ post.url }}">{{post.date | date: "%A, %b %-d, %Y" }}: {{ post.title }} </a>
     </li>
     {% endfor %}
 </ul>
---
+++ new_post.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+
+day="$(date "+%Y-%m-%d-")"
+title="$1"
+ftitle="$day""${title// /_}"".md"
+file="_posts/""$ftitle"
+# touch "$file"
+
+cat << EOF > "$file"
+---
+layout: post
+title: ${title}" 
+category: 
+tags: []
+--- 
+
+# ${title}
+
+EOF
+
+vim "$file"
+
